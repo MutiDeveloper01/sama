457664d6d61c302c8e84ea4ec85125558b508d23
muti <mutideveloper01@gmail.com>
2025-01-11 19:37:06 +0330

    create basic structure project

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+const toggleContainer = document.getElementById('toggle-container');
+
+
+function openToggle(){
+    toggleContainer.style.visibility =  'visible';
+}
+function closeToggle(){
+    toggleContainer.style.visibility = 'hidden';
+}
+
